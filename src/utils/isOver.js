6d52bcfa3a9f4cbc95a14ae4efd5df737e338606
gagaball88 <45629655+gagaball88@users.noreply.github.com>
2022-06-04 18:01:39 +0200

    update to 2.5.3

--- src/utils/isOver.js
+++ src/utils/isOver.js
@@ -1,9 +1,10 @@
 import luxon from "luxon";
 
-export default function isOver(countdownDay, countdownMonth, countdownYear) {
+export default function isOver(countdownHour, countdownDay, countdownMonth, countdownYear) {
     let start = luxon.DateTime.now();
-    let end = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay);
+    let end = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay, countdownHour, 0, 0);
     let ms = end.diff(start, ['milliseconds']).milliseconds.valueOf();
+    console.log('milliseconds until release: ' + ms);
 
     if(ms <= 0) return true;
     else return false;
