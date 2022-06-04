f415a1a1b57e6af3c1f88c3180c8aead495a69c3
gagaball88 <45629655+gagaball88@users.noreply.github.com>
2022-06-04 18:01:39 +0200

    update to 2.5.3

--- src/utils/messageBuilder.js
+++ src/utils/messageBuilder.js
@@ -1,87 +1,83 @@
 import luxon from "luxon";
+import humanize from "humanize-duration";
 
 
-export default function messageBuilder(countdownDay, countdownMonth, countdownYear, mode, accuracy, dayCount, message1, message2, messageEnd) {
+export default function messageBuilder(countdownHour, countdownDay, countdownMonth, countdownYear, mode, accuracy, dayCount, message1, message2, messageEnd) {
+    let newMessageBeta = true;
+    
     let start;
     let end;
+
     
     if(mode === 'countdown') {
-        start = luxon.DateTime.now();
-        end = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay);
+        start = luxon.DateTime.local();
+        end = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay, countdownHour, 0, 0);
     }
     if(mode === 'countup') {
-        start = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay);
-        end = luxon.DateTime.now();
+        start = luxon.DateTime.local(countdownYear, countdownMonth, countdownDay, countdownHour, 0, 0);
+        end = luxon.DateTime.local();
     }
 
     let fullDays = Math.floor(end.diff(start, ['days']).days.valueOf());
     let fullMS = end.diff(start, ['milliseconds']).milliseconds.valueOf();
 
-    let diffDuration = end.diff(start, ['years', 'months', 'days', 'hours', 'minutes', 'seconds']);
-
-    let years = diffDuration.years.valueOf();
-    let months = diffDuration.months.valueOf();
-    let days = diffDuration.days.valueOf();
-    let hours = diffDuration.hours.valueOf();
-    let minutes = diffDuration.minutes.valueOf();
-
     let status = "";
+
+    
     if (fullMS > 0) {
+        status += message1 + " ";
 
-        status += message1;
-
-        if(accuracy >= 0) {
-            if (years > 1) {
-                status += ` ${years} years`;
-            } else if (months === 1) {
-                status += ` ${years} year`;
-            }
-            if(accuracy === 0 && dayCount === true) status += ' (or ' + fullDays + ' days)';
-
+        if (accuracy === 0) {
+            status += humanize(fullMS, {
+                language: "en",
+                serialComma: false,
+                units: ["y"],
+                round: true,
+                conjunction: " and ",
+                })
+        }
+        
+        if (accuracy === 1) {
+            status += humanize(fullMS, {
+                language: "en",
+                serialComma: false,
+                units: ["y", "mo"],
+                round: true,
+                conjunction: " and ",
+                })
         }
 
-        if(accuracy >= 1) {
-            if((accuracy === 2 && days == 0) || (accuracy === 1 && months !== 0) ||(days === 0 && hours === 0 && minutes === 0)) status += " and"
-            if (months > 1) {
-                status += ` ${months} months`;
-            } else if (months === 1) {
-                status += ` ${months} month`;
-            }
-            if(accuracy === 1 && dayCount === true) status += ' (or ' + fullDays + ' days)';
-
+        if (accuracy === 2) {
+            status += humanize(fullMS, {
+                language: "en",
+                serialComma: false,
+                units: ["y", "mo", "d"],
+                round: true,
+                conjunction: " and ",
+                })
         }
 
-        if(accuracy >= 2) {
-            if((accuracy === 3 && hours == 0) || (accuracy === 2 && days !== 0) || (hours === 0 && minutes === 0)) status += " and"
-            if (days > 1) {
-                status += ` ${days} days`;
-            } else if (days === 1) {
-                status += ` ${days} day`;
-            }
-            if(accuracy === 2 && dayCount === true) status += ' (or ' + fullDays + ' days)';
+        if (accuracy === 3) {
+            status += humanize(fullMS, {
+                language: "en",
+                serialComma: false,
+                units: ["y", "mo", "d", "h"],
+                round: true,
+                conjunction: " and ",
+                })
         }
 
-        if(accuracy >= 3) {
-            if((accuracy === 4 && minutes == 0) || (accuracy === 3 && hours !== 0) || (minutes === 0)) status += " and"
-            if (hours > 1) {
-                status += ` ${hours} hours`;
-            } else if (hours === 1) {
-                status += ` ${hours} hour`;
-            }
-            if(accuracy === 3 && dayCount === true) status += ' (or ' + fullDays + ' days)';
-
+        if (accuracy === 4) {
+            status += humanize(fullMS, {
+                language: "en",
+                serialComma: false,
+                units: ["y", "mo", "d", "h", "m"],
+                round: true,
+                conjunction: " and ",
+                })
         }
 
-        if(accuracy >= 4) {
-            if(accuracy === 4 && minutes !== 0) status += " and"
-            if (minutes > 1) {
-                status += ` ${minutes} minutes`;
-            } else if (minutes === 1) {
-                status += ` ${minutes} minute`;
-            }
-            if(accuracy === 4 && dayCount === true) status += ' (or ' + fullDays + ' days)';
-
-        }
+        if (dayCount) status += ' (or ' + fullDays + ' days)';
 
         status += message2;
 
--- src/utils/tweeter.js
+++ src/utils/tweeter.js
@@ -3,17 +3,25 @@ import messageBuilder from"./messageBuilder.js";
 import refreshPic from "./refreshPic.js";
 import player from "play-sound";
 import isOver from "./isOver.js";
+import logger from "./logger.js";
 
-export default async function tweeter(countdownDay, countdownMonth, countdownYear, message1, message2, messageEnd, pictureEnd, mode, accuracy, dayCount, pictureSlot) {
+export default async function tweeter(countdownHour, countdownDay, countdownMonth, countdownYear, message1, message2, messageEnd, pictureEnd, mode, accuracy, dayCount, pictureSlot) {
 
     let picture = refreshPic(pictureSlot);
 
-    if (mode === 'countdown' && isOver()) picture = pictureEnd;
+    if (mode === 'countdown' && isOver(countdownHour, countdownDay, countdownMonth, countdownYear)) picture = pictureEnd;
 
-    let message = messageBuilder(countdownDay, countdownMonth, countdownYear, mode, accuracy, dayCount, message1, message2, messageEnd);
+    let message = messageBuilder(countdownHour, countdownDay, countdownMonth, countdownYear, mode, accuracy, dayCount, message1, message2, messageEnd);
+    console.log(picture);
 
-    await sendTweet(message, picture);
-    player().play('../sounds/notify.mp3');
+    try {
+        await sendTweet(message, picture);
+        player().play('../sounds/notify.mp3');
 
-    //console.log(message + " " + picture);
+        //throw 'MyException';
+    }
+    catch(e) {
+        logger('!!!WARNING!!!\n\nTweet not sent!\n\nPicture used:' + picture + '\n\nError log:')
+        console.log(e);
+    }
 }
